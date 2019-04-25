8249a125f7009e6276ebdf49d244d2dcbee7a9e7
Daniel <danielflorestamayo@gmail.com>
2019-04-24 19:24:28 -0500

    Deber

--- ejercicio3/index.php
+++ ejercicio3/index.php
@@ -11,4 +11,29 @@
  Si los datos son correctos debe redirigir a otra página bienvenido.php que debe imprimir el mensaje "Bienvendo JUAN". (con el nombre de usuario en mayúsculas)
  */
 
+
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <h1 align = "center">EJERCICIO 1</h1>
+	</head>
+	<body>
+   <form method="post">
+     <center>
+     <div>
+     <label>Nombre del usuario : <input type="text" id="usuario" /></label>
+     </div>
+     <br>
+     <div>
+     <label>Contraseña : <input type ="password" name = "pass" ></label>
+     </div>
+     </center>
+      <br>
+      <center><input type="submit" value="INGRESAR"/> </center>
+</form>
+
+	</body>
+</html>	
